4fa01f3172592500a71a20f9956b81d0ab646c12
Manoel Lima <manoelsslima@yahoo.com.br>
2022-12-27 12:20:02 -0400

    Default Pagination of doctor's list

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,8 +25,17 @@ public class MedicoController {
         repository.save(new Medico(dados));
     }
 
+    /**
+     * Use <strong>size</strong> to set the return size. Ex.: size=10 (10 records)<br>
+     * Use <strong>page</strong> to set the page (default is 0). Ex.: page=1<br>
+     * Use <strong>sort</strong> to order the result (sort=fieldname). Ex.: sort=name<br>
+     * Use <strong>,desc</strong> to do a desc sorting (default is asc). Ex.: sort=name,desc<br>
+     * You can ommit @PageableDefault but it can make the API
+     * @param paginacao informações da paginação (size, page, sort)
+     * @return Lista de dados dos médicos
+     */
     @GetMapping
-    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 1, sort = {"nome"}) Pageable paginacao) {
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
